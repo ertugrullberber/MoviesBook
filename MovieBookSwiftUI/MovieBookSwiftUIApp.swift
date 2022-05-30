5a7966b4391ac69eeef77ee4b5612f2a0045913b
ertugrullberber <ertugrulberber3461@gmail.com>
2022-05-30 18:58:31 +0300

    Initial Commit

---
+++ MovieBookSwiftUI/MovieBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookSwiftUIApp.swift
+//  MovieBookSwiftUI
+//
+//  Created by Ertugrul Berber on 30.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
